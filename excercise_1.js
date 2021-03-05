39b902b261267ef28c7cc920679f5025db773d39
unknown <you@example.com>
2021-03-05 00:18:22 -0800

    05-03-2021

---
+++ excercise_1.js
@@ -0,0 +1,145 @@
+// Reverse a string
+('First solution');
+function reverse(str) {
+  const splited = str.split('');
+  const reversed = splited.reverse();
+  const joined = reversed.join('');
+  return joined;
+}
+
+('Second solution');
+function withoutReverse(str) {
+  result = '';
+  for (var i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+}
+console.log(reverse('Vikas'));
+console.log(withoutReverse('Gndec'));
+
+
+// ***********************************************************
+// Searching an element
+function search(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) return i;
+  }
+}
+console.log(search([1, 2, 3, 4, 5], 2));
+
+
+// ***********************************************************
+// Factorial
+function fac(num) {
+  result = 1
+  for(let i=1; i <= num; i++) {
+    result *= i
+  }
+  return result
+}
+console.log(fac(5));
+
+
+// *********************************************************
+// Length of a longest word
+function longestWord(str) {
+  const split = str.split(' ');
+  longest = '';
+  for (const word of split) {
+    if(word.length > longest.length) {
+       longest = word
+    }
+  }
+  return longest.length
+}
+console.log(longestWord('hi im vikas shambhu'));
+
+
+// *********************************************************
+// Palindrome
+function palindrome(str) {
+  const reverse = str.split('').reverse().join('');
+  if (reverse === str) return true;
+  return false;
+}
+console.log(palindrome('racecar'));
+
+
+// ******************************************************************
+// Finding big number in 2D-Array
+function largest(arr) {
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    var maxi = arr[i][0];
+    for (var j = 0; j < arr[i].length; j++) {
+      var currentMaxi = arr[i][j];
+      if (currentMaxi >= maxi) {
+        maxi = currentMaxi;
+      }
+    }
+    result.push(maxi);
+  }
+  return result
+}
+console.log(
+  largest([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [10, 20, 30, -40],
+  ])
+);
+
+
+// ************************************************************
+// First Character to Upper Case
+function titleCase(str) {
+  const res = str
+    .toLowerCase()
+    .split(' ')
+    .map((item) => {
+      return item[0].toUpperCase() + item.slice(1);
+    });
+  return res.join(' ');
+}
+console.log(titleCase('im a vikas from india'));
+
+
+// ************************************************
+// to find senior or junior
+('first solution');
+const open = (data) => {
+  return data.map((item) => {
+    if (item[0] >= 55 && item[1] > 7) {
+      return 'Senior';
+    } else {
+      return 'junior';
+    }
+  });
+};
+('second solution');
+const open = (data) => {
+  return data.map(([age, num]) => (age > 54 && num > 7 ? 'senior' : 'junior'));
+};
+console.log(
+  open([
+    [45, 2],
+    [55, 21],
+    [19, -2],
+  ])
+);
+
+
+// **************************************************
+// get the smallest two numbers & add those two numbers
+function sorted(numbers) {
+  const sortedNum = numbers.sort((a, b) => a - b);
+  const [firstNum, secondNum] = sortedNum;
+  return firstNum + secondNum;
+}
+console.log(sorted([10, 30, 50, 90]));
+
+
+
+
+
